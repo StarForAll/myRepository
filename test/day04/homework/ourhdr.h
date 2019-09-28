60672100b205b2bd905730846b0047a4cf31fc9b
StarForAll <xiong_zhi_cong@163.com>
2019-09-27 20:42:26 -0700

    day04

---
+++ test/day04/homework/ourhdr.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include <string.h>
+#define filePath "/myRepository/test/day01/2.txt" //对应文件的路径
