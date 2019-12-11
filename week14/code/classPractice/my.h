597570cd6bf03680a7b6d98847e9b3bfd58e075c
熊志聪 <xiong_zhi_cong@163.com>
2019-12-11 19:26:15 +0800

    add linux_learn_2

--- week14/code/classPractice/my.h
+++ week14/code/classPractice/my.h
@@ -5,6 +5,10 @@
 #include<limits.h>
 #include<string.h>
 #include<sys/time.h>
+#include<sys/types.h>
 #include<syscall.h>
+#include<errno.h>
 #define NUM 4
+#define LOOP 1000000
 int count(int);
+void *thread_function(void *arg);
